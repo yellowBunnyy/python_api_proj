a1005b1867fc4e9d64772b3ddbd428781b8e83da
yellowBunny <zigggin3@gmail.com>
2022-04-05 15:19:50 +0200

    initial project

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import ADMIN, SQLALCHEMY_URI
+
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
+
+
+def create_app():
+    my_app = Flask(__name__)
+
+    my_app.config["ADMIN"] = ADMIN
+    my_app.config['SQLALCHEMY_URI'] = SQLALCHEMY_URI
+
+    db.init_app(my_app)
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    my_app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    my_app.register_blueprint(main_blueprint)
+    return my_app
--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 from dotenv import dotenv_values
 secrets = dotenv_values()
 ADMIN = secrets["ADMIN"]
-DB = secrets["SECRET"]
+SQLALCHEMY_URI = secrets["SQLALCHEMY_URI"]
